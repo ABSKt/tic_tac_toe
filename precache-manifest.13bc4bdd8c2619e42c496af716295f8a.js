6b371fd589eddbcbbb60113548db1c3081673244
Abhishek Thakur <abhishek.thakur@crewkarma.com>
2020-06-28 15:17:09 +0530

    Updates

---
+++ precache-manifest.13bc4bdd8c2619e42c496af716295f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8114bad974ed3f05417ff37b97f2893",
+    "url": "/tic_tac_toe/index.html"
+  },
+  {
+    "revision": "f075f400412dcdb475d0",
+    "url": "/tic_tac_toe/static/css/main.96dbc504.chunk.css"
+  },
+  {
+    "revision": "c047f265c1f1e298f66b",
+    "url": "/tic_tac_toe/static/js/2.4c08fcdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic_tac_toe/static/js/2.4c08fcdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f075f400412dcdb475d0",
+    "url": "/tic_tac_toe/static/js/main.11f744b3.chunk.js"
+  },
+  {
+    "revision": "135ba7b087ac225cebf9",
+    "url": "/tic_tac_toe/static/js/runtime-main.49020b15.js"
+  }
+]);
